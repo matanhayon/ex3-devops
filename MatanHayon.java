e0293b55eea3d679139d33074ce4e386236ab4a7
matanhayon <matanhayon@gmail.com>
2024-05-11 17:11:27 +0300

    changed MatanHayon.java to print about step 8

--- MatanHayon.java
+++ MatanHayon.java
@@ -4,7 +4,7 @@ public class MatanHayon
     {
         for(int i =1;i<=10; i++)
         {
-            System.out.println("Matan Hayon ... " + i);
+            System.out.println("Changes of step 6 ... " + i);
         }
     }
 }
